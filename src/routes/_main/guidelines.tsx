e49b50919a3b9ff790c6afc742ebccd7ae63329c
Pavel Krutov <pavel@krutov.eu>
2025-12-01 13:06:20 +0000

    feat: add full guidelines

--- src/routes/_main/guidelines.tsx
+++ src/routes/_main/guidelines.tsx
@@ -1,16 +1,21 @@
 import { createFileRoute } from '@tanstack/react-router'
 import {PageHeader} from "@/components/features/page-header.tsx";
 import {BookIcon, WarningIcon} from "@phosphor-icons/react";
-import {Button} from "@/components/ui/button.tsx";
 import {Accordion} from "@/components/ui/accordion.tsx";
+import ConnectionRules from "@/components/features/guidelines/connection.tsx";
+import ServerRules from "@/components/features/guidelines/server.tsx";
+import ProjectRules from "@/components/features/guidelines/projects.tsx";
+import DiscordRules from "@/components/features/guidelines/discord.tsx";
+import EventsRules from "@/components/features/guidelines/events.tsx";
+import ServerAddons from "@/components/features/guidelines/addons.tsx";
 
 export const Route = createFileRoute('/_main/guidelines')({
   component: Guidelines,
 })
 
-export default function Guidelines() {
+function Guidelines() {
     return (
-        <section className="mx-5 grid items-center gap-6 pt-6 md:mx-10 md:pb-10 xl:mx-20">
+        <section className="mx-5 grid items-center gap-6 pt-6 md:mx-10 xl:mx-20">
             <PageHeader
                 icon={BookIcon}
                 title="Guidelines"
@@ -20,28 +25,18 @@ export default function Guidelines() {
                     { label: 'Home', href: '/' },
                     { label: 'Guidelines' }
                 ]}
-                actions={
-                    <div className="flex gap-2">
-                        <Button variant="outline" size="sm">
-                            Share
-                        </Button>
-                        <Button size="sm">
-                            Edit
-                        </Button>
-                    </div>
-                }
             />
 
             <Accordion defaultValue={'getting-started'} type="single" collapsible className="w-full space-y-2">
-                {/*<ConnectionRules/>*/}
-                {/*<ServerRules/>*/}
-                {/*<ProjectRules/>*/}
-                {/*<DiscordRules/>*/}
-                {/*<EventsRules/>*/}
-                {/*<ServerAddons/>*/}
+                <ConnectionRules/>
+                <ServerRules/>
+                <ProjectRules/>
+                <DiscordRules/>
+                <EventsRules/>
+                <ServerAddons/>
             </Accordion>
 
-            <div className="flex items-start gap-1.5 text-xs sm:text-sm text-muted-foreground bg-muted/20 px-2 mb-2 py-1.5 sm:px-3 sm:py-2 rounded-sm border">
+            <div className="flex items-start gap-1.5 text-xs sm:text-sm text-muted-foreground bg-muted/20 px-2 py-1.5 sm:px-3 sm:py-2 rounded-sm border">
                 <WarningIcon weight={'duotone'} className="size-3 sm:size-4 text-amber-500 mt-0.5 flex-shrink-0"/>
                 <span className="leading-tight">
                 <strong className="text-foreground">Note:</strong> These rules are strictly enforced.

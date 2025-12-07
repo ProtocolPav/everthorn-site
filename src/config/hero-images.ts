59aad4dce1fe051c83f74a41ae7f5c7f9624aeab
Pavel <pbkrutov@gmail.com>
2025-12-08 01:45:48 +0200

    refactor(landing-page): add new hero images

--- src/config/hero-images.ts
+++ src/config/hero-images.ts
@@ -6,9 +6,10 @@ type heroImage = {
 }
 
 export const hero_images: heroImage[] = [
-    { name: 'Spawn Village', image: '/landing/spawn_village.png', href: '/wiki/spawn_village' },
-    { name: 'Padova', image: '/landing/padova.png', href: '/wiki/padova' },
-    { name: 'Solaris', image: '/landing/solaris.png', href: '/wiki/solaris' },
-    { name: 'Proving Grounds', image: '/landing/provingground.png', href: '/wiki/proving_grounds' },
-    { name: 'Shroomland', image: '/landing/shroomland.png', href: '/wiki/shroomland' },
+    { name: 'Spawn Village', image: '/landing/spawn.png', href: '/wiki/spawn_village' },
+    { name: 'Gal Daral', image: '/landing/gal_daral.png', href: '/wiki/gal_daral' },
+    { name: 'Ceras', image: '/landing/ceras.png', href: '/wiki/ceras' },
+    { name: 'Redbeard Industries', image: '/landing/redbeard_industries.png', href: '/wiki/redbeard_industries' },
+    { name: 'Lost Creek', image: '/landing/lost_creek.png', href: '/wiki/lost_creek' },
+    { name: 'The Athenaeum', image: '/landing/athenaeum.png', href: '/wiki/the_athenaeum' },
 ]

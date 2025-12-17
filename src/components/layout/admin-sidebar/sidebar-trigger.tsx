c022877ce5711e26997470c2f904f80868ce1662
Pavel <pbkrutov@gmail.com>
2025-12-17 18:28:32 +0200

    refactor: optimise Sidebar Trigger to always show on mobile

--- src/components/layout/admin-sidebar/sidebar-trigger.tsx
+++ src/components/layout/admin-sidebar/sidebar-trigger.tsx
@@ -5,10 +5,10 @@ import {cn} from "@/lib/utils.ts";
 import {Separator} from "@/components/ui/separator.tsx";
 
 export function AdminSidebarTrigger() {
-    const { toggleSidebar, open } = useSidebar()
+    const { toggleSidebar, open: openDesktop, isMobile } = useSidebar()
 
     return (
-        <div className={cn(open ? "hidden" : "flex items-center gap-2")}>
+        <div className={cn((openDesktop && !isMobile) ? "hidden" : "flex items-center gap-2")}>
             <Button
                 size={'icon'}
                 variant={'ghost'}

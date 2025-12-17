e17b09ec759c3646af6a48a9e35192a7a60496f0
Pavel <pbkrutov@gmail.com>
2025-12-17 17:23:35 +0200

    refactor: refine header

--- src/components/layout/admin-sidebar/header.tsx
+++ src/components/layout/admin-sidebar/header.tsx
@@ -1,28 +1,32 @@
-import { ChevronsUpDown,  } from "lucide-react"
-
 import {
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
+    useSidebar
 } from "@/components/ui/sidebar"
 import logo from "/everthorn.png";
+import {ArrowLineLeftIcon} from "@phosphor-icons/react";
 
 export function AdminHeader() {
+    const { toggleSidebar } = useSidebar()
+
     return (
         <SidebarMenu>
             <SidebarMenuItem>
                 <SidebarMenuButton
                     size="lg"
                     className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                    onClick={toggleSidebar}
                 >
-                    <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                        <img src={logo} alt="Everthorn Logo" className="size-4" />
+                    <div className="text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                        <img src={logo} alt="Everthorn Logo" className="size-6" />
                     </div>
                     <div className="grid flex-1 text-left text-sm leading-tight">
                         <span className="truncate font-medium">Everthorn</span>
-                        <span className="truncate text-xs">[Thingy]</span>
+                        <span className="truncate text-xs">Admin Dashboard</span>
                     </div>
-                    <ChevronsUpDown className="ml-auto" />
+
+                    <ArrowLineLeftIcon className={'mr-1'} weight={'bold'} />
                 </SidebarMenuButton>
             </SidebarMenuItem>
         </SidebarMenu>

8ef6e2a39c458d75b1c79661d71db95104bff90e
Pavel <pbkrutov@gmail.com>
2025-12-30 18:38:54 +0200

    refactor: add border

--- src/routes/admin/map.tsx
+++ src/routes/admin/map.tsx
@@ -13,8 +13,9 @@ function LiveMap() {
     return (
         <div
             style={{ width: '100%', height: 'calc(100vh - 3.6rem)'}}
+            className={'p-1'}
         >
-            <div className={'h-full w-full border-2  border-orange-400'}>
+            <div className={'h-full w-full border-2 border-orange-400 rounded-lg overflow-hidden'}>
                 <WorldMap />
             </div>
         </div>

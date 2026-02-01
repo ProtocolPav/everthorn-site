1784e11cf6122c49efc05d41bdee3370d30edf4d
Pavel Krutov <pavel@krutov.eu>
2026-02-01 17:43:54 +0000

    refactor: edit objective types

--- src/config/quest-form-options.ts
+++ src/config/quest-form-options.ts
@@ -1,9 +1,10 @@
 import {SeamlessSelectOption} from "@/components/features/common/seamless-select.tsx";
 import {
+    AlienIcon,
     CheckCircleIcon,
-    ClockCounterClockwiseIcon,
-    HandWavingIcon,
-    SealQuestionIcon,
+    ClockCounterClockwiseIcon, CommandIcon, CursorClickIcon,
+    HandWavingIcon, MapPinAreaIcon,
+    SealQuestionIcon, ShovelIcon, SwordIcon,
 } from "@phosphor-icons/react";
 
 export const QUEST_TYPES: SeamlessSelectOption[] = [
@@ -43,30 +44,41 @@ export const QUEST_TYPES: SeamlessSelectOption[] = [
 
 export const OBJECTIVE_TYPES: SeamlessSelectOption[] = [
     {
-        value: 'minor',
-        label: 'Minor Quest',
-        icon: HandWavingIcon
+        value: 'kill',
+        label: 'Kill',
+        icon: SwordIcon
     },
     {
-        value: 'story',
-        label: 'Story Quest',
-        icon: CheckCircleIcon,
-        triggerClassName: "bg-violet-500/10 text-violet-700 border-violet-500/20 hover:bg-violet-500/20 dark:text-violet-300 dark:border-violet-500/30",
-        iconClassName: "text-violet-600 dark:text-violet-400"
+        value: 'mine',
+        label: 'Mine',
+        icon: ShovelIcon
     },
     {
-        value: 'weekly',
-        label: 'Weekly Quest',
-        icon: SealQuestionIcon,
-        triggerClassName: "bg-sky-500/10 text-sky-700 border-sky-500/20 hover:bg-sky-500/20 dark:text-sky-300 dark:border-sky-500/30",
-        iconClassName: "text-sky-600 dark:text-sky-400"
-    },
-    {
-        value: 'side',
-        label: 'Side Quest',
-        icon: ClockCounterClockwiseIcon,
-        triggerClassName: "bg-sky-500/10 text-sky-700 border-sky-500/20 hover:bg-sky-500/20 dark:text-sky-300 dark:border-sky-500/30",
-        iconClassName: "text-sky-600 dark:text-sky-400",
+        value: 'visit',
+        label: 'Visit',
+        icon: MapPinAreaIcon,
         disabled: true
     },
+    {
+        value: 'interact',
+        label: 'Interact',
+        icon: CursorClickIcon,
+        disabled: true
+    },
+    {
+        value: 'boss',
+        label: 'Boss',
+        icon: AlienIcon,
+        triggerClassName: "bg-red-500/10 text-red-700 border-red-500/20 hover:bg-red-500/20 dark:text-red-300 dark:border-red-500/30",
+        iconClassName: "text-red-600 dark:text-red-400",
+        disabled: true
+    },
+    {
+        value: 'scriptevent',
+        label: 'Custom Script',
+        icon: CommandIcon,
+        triggerClassName: "bg-orange-500/10 text-orange-700 border-orange-500/20 hover:bg-orange-500/20 dark:text-orange-300 dark:border-orange-500/30",
+        iconClassName: "text-orange-600 dark:text-orange-400",
+        info: "For fine-grained customization using command blocks"
+    }
 ];

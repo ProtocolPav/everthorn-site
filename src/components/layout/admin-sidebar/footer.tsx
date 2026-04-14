f15e75fd0d51797526a0631e8125b4ad9b084c2e
Pavel Krutov <pavel@krutov.eu>
2026-04-14 23:40:32 +0300

    feat: enhance admin footer with user details and Discord integration
    - Add dynamic username and role display using `useEverthornMember`.
    - Integrate `DiscordButton` for improved user engagement.
    - Adjust layout for better handling of collapsed and expanded sidebar states.

--- src/components/layout/admin-sidebar/footer.tsx
+++ src/components/layout/admin-sidebar/footer.tsx
@@ -2,11 +2,36 @@ import {
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
+    useSidebar
 } from "@/components/ui/sidebar"
 import {SignOutIcon} from "@phosphor-icons/react";
 import {Link} from "@tanstack/react-router";
+import {DiscordButton} from "@/components/features/discord-button/discord.tsx";
+import {useEverthornMember} from "@/hooks/use-everthorn-member.ts";
 
 export function AdminFooter() {
+    const { state } = useSidebar()
+    const everthornMember = useEverthornMember()
+
+    if (state === "collapsed") {
+        return (
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <Link to={'/'}>
+                        <SidebarMenuButton
+                            size="lg"
+                            variant={'default'}
+                        >
+                            <div className="flex aspect-square size-8 items-center justify-center rounded-lg">
+                                <SignOutIcon weight={'bold'} />
+                            </div>
+                        </SidebarMenuButton>
+                    </Link>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        )
+    }
+
     return (
         <SidebarMenu>
             <SidebarMenuItem>
@@ -15,11 +40,13 @@ export function AdminFooter() {
                         size="lg"
                         variant={'default'}
                     >
+                        <DiscordButton />
+                        <div className="grid flex-1 text-left text-sm leading-tight">
+                            <span className="truncate font-medium">{everthornMember.thornyUser?.username}</span>
+                            <span className="truncate text-xs text-muted-foreground">{everthornMember.thornyUser?.role || 'No Role'}</span>
+                        </div>
                         <div className="flex aspect-square size-8 items-center justify-center rounded-lg">
                             <SignOutIcon weight={'bold'} />
-                        </div>
-                        <div className="grid flex-1 text-left text-sm leading-tight">
-                            <span className="truncate font-medium">Let Me Out!</span>
                         </div>
                     </SidebarMenuButton>
                 </Link>

2ff600ffe580603e7105bcf467afcb90d1ea8900
Pavel Krutov <pavel@krutov.eu>
2026-07-16 22:14:37 +0100

    chore: add package and icons

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ const config = defineConfig({
         tanstackStart(),
         viteReact(),
     ],
+    optimizeDeps: {
+        exclude: ['@resvg/resvg-js', '@resvg/resvg-js-darwin-arm64'],
+    },
 })
 
 export default config

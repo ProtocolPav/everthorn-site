12ab56f8e811433db95837d4a0a904f6747589cc
Pavel Krutov <pavel@krutov.eu>
2026-07-23 10:49:30 +0100

    fix: balance not properly being converted in quest-schema-conversion.ts

--- src/lib/quest-schema-conversion.ts
+++ src/lib/quest-schema-conversion.ts
@@ -2,16 +2,22 @@ import {QuestFormValues} from "@/lib/schemas/quest-form.tsx";
 import {QuestOut} from "@/api/nexuscore/model";
 
 export function convertApiToZod(quest: QuestOut): QuestFormValues {
-    const { start_time, end_time, created_by, ...rest } = quest;
+    const { start_time, end_time, created_by, objectives, ...rest } = quest;
 
-    // @ts-ignore
     return {
         range: {
             start: start_time,
             end: end_time
         },
         created_by: created_by.thorny_id,
-        ...rest
+        ...rest,
+        objectives: objectives.map(objective => ({
+            ...objective,
+            rewards: objective.rewards.map(reward => ({
+                ...reward,
+                isBalance: reward.balance !== null && reward.balance !== undefined
+            }))
+        }))
     } as QuestFormValues;
 }
 

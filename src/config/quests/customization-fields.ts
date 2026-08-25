516aea356ff448d5a393efe499582aaded26e06c
Pavel Krutov <pavel@krutov.eu>
2026-08-25 15:06:24 +0100

    feat: add waypoint customization and improve Customizations type handling
    Adds new "Waypoint" customization under the "Immersion" section with support for audiovisual customizations. Refactors `Customizations` type structure for improved type safety and readability.

--- src/config/quests/customization-fields.ts
+++ src/config/quests/customization-fields.ts
@@ -1,12 +1,12 @@
 import type { ComponentType } from "react";
-import type { CustomizationId } from "@/config/quests/customization-options.ts";
 import { NaturalBlocksField } from "@/components/features/quests/fields/customization/natural-blocks-field.tsx";
 import { LocationField } from "@/components/features/quests/fields/customization/location-field.tsx";
 import { MainhandField } from "@/components/features/quests/fields/customization/mainhand-field.tsx";
 import { TimerField } from "@/components/features/quests/fields/customization/timer-field.tsx";
 import { MaximumDeathsField } from "@/components/features/quests/fields/customization/maximum-deaths-field.tsx";
+import {Customizations} from "@/api/nexuscore/model";
 
-export const CUSTOMIZATION_FIELD_MAP: Record<CustomizationId, ComponentType> = {
+export const CUSTOMIZATION_FIELD_MAP: Record<keyof Customizations, ComponentType> = {
     natural_block: NaturalBlocksField,
     location: LocationField,
     mainhand: MainhandField,
